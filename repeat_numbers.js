f01ea7197d039494744762d691a5ca552af3d279
Dominic Tremblay <dominictremblay24@gmail.com>
2020-08-24 12:23:27 -0400

    final demo -- added back the try catch to repeat numbers

--- repeat_numbers.js
+++ repeat_numbers.js
@@ -1,3 +1,4 @@
+const assert = require('assert').strict;
 
 const repeatNumbers = function (data) {
   // Put your solution here
@@ -16,3 +17,46 @@ const repeatNumbers = function (data) {
 
   return num.join(', ');
 };
+
+try {
+  assert.equal(
+    repeatNumbers([[1, 10]]),
+    '1111111111',
+    "it should be '1111111111' with the input array [[1, 10]]"
+  );
+} catch (err) {
+  console.log(
+    `${err.message}, actual: ${err.actual} expected: ${err.expected}`
+  );
+}
+
+try {
+  assert.equal(
+    repeatNumbers([
+      [1, 2],
+      [2, 3],
+    ]),
+    '11, 222',
+    "it should be '11, 222' with the input array '[[1, 2],[2, 3]]'"
+  );
+} catch (err) {
+  console.log(
+    `${err.message}, actual: ${err.actual} expected: ${err.expected}`
+  );
+}
+
+try {
+  assert.equal(
+    repeatNumbers([
+      [10, 4],
+      [34, 6],
+      [92, 2],
+    ]),
+    '10101010, 343434343434, 9292',
+    "it should be '10101010, 343434343434, 9292' with the input array '[[10, 4],[34, 6],[92, 2]]'"
+  );
+} catch (err) {
+  console.log(
+    `${err.message}, actual: ${err.actual} expected: ${err.expected}`
+  );
+}
