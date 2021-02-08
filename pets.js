893b590675a6f65769a012e34dd5edb5fa74f137
Dominic Tremblay <dominictremblay24@gmail.com>
2021-02-08 09:40:05 -0500

    new demo reset

---
+++ pets.js
@@ -0,0 +1,24 @@
+const pets = [
+  {
+    name: 'Gus',
+    gender: 'Male',
+    breed: 'Domestic Cat',
+    type: '🐱',
+    age: 8,
+  },
+  {
+    name: 'Coco',
+    gender: 'Female',
+    breed: 'Russell Terrier',
+    type: '🐶',
+    age: 5,
+  },
+];
+
+const newPet = {
+  name: 'Rosie',
+  gender: 'Female',
+  breed: 'Domestic Cat',
+  type: '🐱',
+  age: 2
+}
